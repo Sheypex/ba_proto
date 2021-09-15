43a2c5a3e21e02b947ba824b32a8a3e019b44c1c
Sheypex <sheypex@gmail.com>
2021-09-15 15:01:29 +0200

    prepping recSheduling of cvmodels

--- py/recommenderScheduler.py
+++ py/recommenderScheduler.py
@@ -758,8 +758,8 @@ def main():
         #
         def dumpResults():  # TODO this needs to perform a sanit-check on the data that is to be dumped since this also runs on error so the results may be corrupted!
             rc.log(f"Going to save to {pFile}")
-            with open(pFile, 'bw') as f:
-                pickle.dump(times, f)
+            with open(pFile, 'bw') as f2:
+                pickle.dump(times, f2)
             rc.log("oh no no no no")
             rc.log(f"Saved to {pFile}")
 
